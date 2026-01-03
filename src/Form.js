b58765ea34ac4ec7a45632917a93f0934792e0f5
KethraPackiamB <kethrapackiamb@gmail.com>
2026-01-03 16:37:37 +0530

    Created Registration Form

---
+++ src/Form.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Form = () => {
+const [data, setData] = useState({
+        firstName : "",
+        lastName : "",
+        city : "",
+        country : "",
+    });
+
+    const handleChange = (e) => {
+        const key = e.target.name;
+        setData({...data, [key]:e.target.value});
+        console.log(`${key} : ${e.target.value}`);
+    }
+
+    const handleChanges = (e,key) => {
+        setData({...data, [key]:e.target.value});
+        }
+
+   
+
+    
+return(
+        <div className="form-fields">
+            <div> 
+                <label htmlFor="firstName">First Name :</label>
+                <input id="firstName" value={data.firstName} name="firstName" onChange={e =>handleChanges(e,"firstName")}/>
+            </div>
+            <div> 
+                <label htmlFor="lastName">Last Name :</label>
+            <input id="lastName" name="lastName" value={data.lastName} onChange={handleChange}/>
+            </div>
+           <div> 
+            <label htmlFor="city">City :</label>
+            <input id="city" name="city" value={data.city} onChange={handleChange} />
+            </div>
+           <div>
+            <label>Country :</label>
+            <input id="country" name="country" value={data.country} onChange={handleChange}/>
+            </div>
+            
+            </div>
+    )
+}
+export default Form;
